c7a6f10ef31a7734c56ac63c97588b5931f93f5b
brenosilldev <brenosill@hotmail.com>
2026-07-17 00:45:27 -0300

    feat: nova aulas

--- basics/2.aula.py
+++ basics/2.aula.py
@@ -12,9 +12,17 @@
 
 todos = []
 for i , x in enumerate(range(1,21,1)):
-
     if x % 2 == 0:
-        print(f"i: {i} - x: {x}")
+        print(f"i: {i} - x: {str(x).capitalize()}")
+
+for i in range(1,21):
+    print(f"{i} - x: {str(i).capitalize()}")
 
 
 print(f"Todos os números pares de 1 a 20: {todos}")
+
+
+
+nome = str(input("Digite um nome para sair: "))
+while True:
+    print(nome.capitalize().title())
